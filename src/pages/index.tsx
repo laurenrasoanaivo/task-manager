4ee9a0166dec83e28ea3252ae8402a65b246f777
laurenrasoanaivo <hei.lauren.3@gmail.com>
2023-06-15 09:45:46 +0300

    Partie Rendu côté serveur et affichage de la différence horaire

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,62 @@
+import { useEffect, useState } from 'react';
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-/**
-  Calculates the time difference between the server time and client time.
-  @param {Date} serverTime - The server time.
-  @param {Date} clientTime - The client time.
-  @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
-*/
-const calculateTimeDifference = (server: Date, client: Date) => {};
-
+const calculateTimeDifference = (server: Date, client: Date): string => {
+  const timeDifference = Math.abs(client.getTime() - server.getTime());
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+};
 
 export default function Home() {
   const router = useRouter();
+  const [clientTime, setClientTime] = useState<Date | null>(null);
+  const [serverTime, setServerTime] = useState<Date | null>(null);
+  const [timeDifference, setTimeDifference] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Set client time
+    const clientDateTime = new Date();
+    setClientTime(clientDateTime);
+  }, []);
+
+  useEffect(() => {
+    // Set server time
+    const serverDateTime = new Date();
+    setServerTime(serverDateTime);
+  }, []);
+
+  useEffect(() => {
+    if (clientTime && serverTime) {
+      const difference = calculateTimeDifference(serverTime, clientTime);
+      setTimeDifference(difference);
+    }
+  }, [clientTime]);
+
+  useEffect(() => {
+    // Update time difference whenever the client time changes
+    const updateTimeDifference = () => {
+      const newClientTime = new Date();
+      setClientTime(newClientTime);
+    };
+
+    // Listen for the "onfocus" event to detect when the window gains focus
+    window.addEventListener("focus", updateTimeDifference);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("focus", updateTimeDifference);
+    };
+  }, []);
+
   const moveToTaskManager = () => {
     router.push("/tasks");
   }
+
   return (
     <>
       <Head>
@@ -26,16 +68,18 @@ export default function Home() {
       <main>
         <h1>The easiest exam you will ever find</h1>
         <div>
-          {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
+          {/* Display client time (DD-MM-YYYY HH:mm) */}
           <p>
-            Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            Client time:{" "}
+            <span className="clientTime">
+              {clientTime?.toLocaleString()}
+            </span>
           </p>
 
-          {/* Display here the time difference between the server side and the client side */}
+          {/* Display time difference between client and current time */}
           <p>
             Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="timeDifference">{timeDifference}</span>
           </p>
         </div>
 
