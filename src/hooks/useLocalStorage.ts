472e38e9e1a3560217cf2bae61d48752ddcbab4a
laurenrasoanaivo <hei.lauren.3@gmail.com>
2023-06-15 11:46:17 +0300

    Création d'un hook pour interagir avec le Local Storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,23 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from 'react';
 
-}
+const isBrowser = typeof window !== 'undefined';
 
-export {
-  useLocalStorage
-}
+const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T) => void] => {
+  const [value, setValue] = useState<T>(() => {
+    if (isBrowser) {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialValue;
+    }
+    return initialValue;
+  });
+
+  useEffect(() => {
+    if (isBrowser) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export { useLocalStorage };
