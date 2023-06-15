7da2c3cfe6fb43e32d266e13245f2cd80c2d9119
laurenrasoanaivo <hei.lauren.3@gmail.com>
2023-06-15 11:45:43 +0300

    Création du store en utilisant Zustand pour gérer les tâches

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,43 @@
-const useTaskManager = () => {
+import create from 'zustand';
 
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-export {
-  useTaskManager
+interface TaskManagerState {
+  tasks: Task[];
+  searchTask: string;
+  addTask: (newTask: Task) => void;
+  updateTask: (taskId: number, updatedTask: Partial<Task>) => void;
+  deleteTask: (taskId: number) => void;
+  setSearchTask: (search: string) => void;
 }
+
+const useTaskManager = create<TaskManagerState>((set) => ({
+  tasks: [],
+  searchTask: '',
+  addTask: (newTask) => {
+    set((state) => ({
+      tasks: [...state.tasks, newTask],
+    }));
+  },
+  updateTask: (taskId, updatedTask) => {
+    set((state) => ({
+      tasks: state.tasks.map((task) =>
+        task.id === taskId ? { ...task, ...updatedTask } : task
+      ),
+    }));
+  },
+  deleteTask: (taskId) => {
+    set((state) => ({
+      tasks: state.tasks.filter((task) => task.id !== taskId),
+    }));
+  },
+  setSearchTask: (search) => {
+    set({ searchTask: search });
+  },
+}));
+
+export { useTaskManager };
